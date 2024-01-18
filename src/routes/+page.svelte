f19fbe0071a29e63d4af8b92b4edbca233555b03
onow <oliwierjnowak@gmail.com>
2024-01-18 11:35:37 +0100

    listbox item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script context="module" lang="ts">
 	import { fetchData}  from '../graphql/fetchData';
 	import type {random, user, tweet } from '../graphql/fetchData';
-	import { Autocomplete,popup } from '@skeletonlabs/skeleton';
-	import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
-
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 </script>
 
 
@@ -17,7 +15,6 @@
 	onMount(async () => {
 		try {
 			userData = await fetchData() ;
-			console.log(userData.queryUser[0].name)
 			usernames = userData.queryUser.map(x => x.name)
 		
 		/*	 flavorOptions = userData.queryUser.map(user => ({
@@ -36,28 +33,9 @@
   });
 	let a = [1,2,3,4,5]
 
-
-	// Local
-	let inputPopupDemo = '';
-	let popupSettings: PopupSettings = {
-		event: 'focus-click',
-		target: 'popupAutocomplete',
-		placement: 'bottom'
-	};
-
-	let inputDemo = '';
-	let inputAllowlist = '';
-	type FlavorOption = AutocompleteOption<string, { healthy: boolean }>;
-	const flavorOptions: FlavorOption[] = [
-		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
-		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
-		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
-		{ label: 'Neapolitan', value: 'neapolitan', keywords: 'mix, strawberry, chocolate, vanilla', meta: { healthy: false } },
-		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
-		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
-	];
-	function onPopupDemoSelect(event: CustomEvent<FlavorOption>): void {
-		inputPopupDemo = event.detail.label;
+	let valueSingle: string ;
+	function showSelected(){
+		console.log(valueSingle)
 	}
 </script>
 
@@ -91,10 +69,15 @@
 	  <!-- Content for the third container -->
 
 	  {#if loaded == true}
-	  <p>true</p>
-	  <h1>{userData.queryUser[0].name}</h1>
-	
+	  <ListBox >
+	  {#each userData.queryUser as user }
 
+	  	<ListBoxItem bind:group={valueSingle} name="medium" on:change={showSelected}  value={user.id}>{user.name}</ListBoxItem>
+	
+	  {/each}	
+	 
+	</ListBox>
+				
   {:else}
 	  <p>false</p>
   {/if}
@@ -103,16 +86,3 @@
   </div>
 
 
-  <div class="text-token w-full max-w-sm space-y-2">
-	<input
-		class="input autocomplete"
-		type="search"
-		name="autocomplete-search"
-		bind:value={inputPopupDemo}
-		placeholder="Search..."
-		use:popup={popupSettings}
-	/>
-	<div data-popup="popupAutocomplete" class="card w-full max-w-sm max-h-48 p-4 overflow-y-auto" tabindex="-1">
-		<Autocomplete bind:input={inputPopupDemo} options={flavorOptions} on:selection={onPopupDemoSelect} />
-	</div>
-</div>
